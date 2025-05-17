b05d375d56d6ec76db468ce798d915b666918826
Ricardo Collado Rothschild <ricardocolladorothschild@gmail.com>
2025-05-17 19:14:10 -0400

    first commit

---
+++ src/main/java/com/employeeData/EmployeeRegistrationSystemCORE/EmployeeRegistrationSystemCoreApplication.java
@@ -0,0 +1,13 @@
+package com.employeeData.EmployeeRegistrationSystemCORE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationSystemCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationSystemCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeeData/EmployeeRegistrationSystemCORE/EmployeeRegistrationSystemCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeData.EmployeeRegistrationSystemCORE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationSystemCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
